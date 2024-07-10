6cd9d95e68ca0192d891f2e7d745c1d2ce48de5c
Chao-Jen Wong <chao-jen.wong@seattlechildrens.org>
2024-07-10 16:17:42 -0700

    remove lib/components associated with projects

--- web/src/lib/components/index.js
+++ web/src/lib/components/index.js
@@ -1,9 +1,6 @@
 import Input from "./Input.svelte";
 import Modal from "./Modal.svelte";
-import MyProjectItem from "./MyProjectItem.svelte";
 import MyNGSPostItem from "./MyNGSPostItem.svelte";
 import TextArea from "./TextArea.svelte";
-import ProjectCard from "./ProjectCard.svelte";
-import ProjectItem from "./ProjectItem.svelte";
 import NGSPostItem from "./NGSPostItem.svelte";
-export { Input, Modal, MyProjectItem, MyNGSPostItem, ProjectCard, TextArea, ProjectItem, NGSPostItem };
+export { Input, Modal, MyNGSPostItem,TextArea, NGSPostItem };
